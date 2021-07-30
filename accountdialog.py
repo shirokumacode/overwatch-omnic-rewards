78e16fa2598b064b40583c1923abd5d77973a592
shirokumacode <shiro_code@protonmail.com>
2021-07-30 16:29:42 +0100

    Use HTTPS on urls

--- accountdialog.py
+++ accountdialog.py
@@ -16,8 +16,8 @@ class AccountDialog(QDialog):
 
         outer_layout = QVBoxLayout()
 
-        blizzard_url = "<a href=\"http://www.blizzard.com\">blizzard.com</a>"
-        blizzard_user_url = "<a href=\"http://www.blizzard.com/user\">blizzard.com/user</a>"
+        blizzard_url = "<a href=\"https://www.blizzard.com\">blizzard.com</a>"
+        blizzard_user_url = "<a href=\"https://www.blizzard.com/user\">blizzard.com/user</a>"
 
         instructions_text = (
             f"<h3 style=\"text-align:center\">How to get your Blizzard user ID</h3>\n"
