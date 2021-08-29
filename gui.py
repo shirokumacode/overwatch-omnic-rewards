45a095bddf3575be6a809573023b9177e927c503
shirokumacode <shiro_code@protonmail.com>
2021-08-29 20:19:09 +0100

    Cli version with --cli argument

--- gui.py
+++ gui.py
@@ -6,42 +6,60 @@ import argparse
 import logging
 import os
 import sys
+import signal
 
 from stats import Stats
 from settings import SettingsManager
 from systemtray import SystemTray
+from cli import CLIApp
 
 logger = logging.getLogger(__name__)
 
 
 def main():
+    # Arg Parser and Logging
     options, qt_args = arg_parse()
     application_path = get_application_path()
     configure_logging(options, application_path)
 
+    # Create Settings and Stats
     config_location = os.path.join(application_path, options.config)  # config.json
     history_location = os.path.join(application_path, options.history)  # history.csv
-
     settings = SettingsManager(config_location)
     stats = Stats(history_location)
-
-    app = QApplication(sys.argv[:1] + qt_args)
-
-    logger.debug(f'QT Desktop session: sessionId="{app.sessionId()}", sessionKey="{app.sessionKey()}"')
-
-    if not QSystemTrayIcon.isSystemTrayAvailable():
-        QMessageBox.critical(None, 'System tray not found', 'Can\'t start app. No system tray found')
-        sys.exit(1)
-
-    QApplication.setQuitOnLastWindowClosed(False)
 
     if options.debug:
         set_local_urls()
 
-    # Create the tray
-    tray = SystemTray(settings, stats, quiet_mode=options.quiet, parent=app)
-    app.aboutToQuit.connect(tray.prepare_to_exit)
-    app.commitDataRequest.connect(tray.prepare_to_exit)
+    # Create Qt Application
+    if options.cli:
+        logger.info("CLI Mode enabled")
+        app = QCoreApplication(sys.argv[:1] + qt_args)
+
+        # Exit Application using signals hack
+        # https://stackoverflow.com/questions/4938723/what-is-the-correct-way-to-make-my-pyqt-application-quit-when-killed-from-the-co
+        signal.signal(signal.SIGINT, lambda *a: app.quit())
+        signal.signal(signal.SIGTERM, lambda *a: app.quit())
+        timer = QTimer()
+        timer.start(500)  # You may change this if you wish.
+        timer.timeout.connect(lambda: None)
+
+        cli = CLIApp(settings, stats, parent=app)
+        app.aboutToQuit.connect(cli.prepare_to_exit)
+
+    else:
+        app = QApplication(sys.argv[:1] + qt_args)
+        logger.debug(f'QT Desktop session: sessionId="{app.sessionId()}", sessionKey="{app.sessionKey()}"')
+
+        if not QSystemTrayIcon.isSystemTrayAvailable():
+            QMessageBox.critical(None, 'System tray not found', 'Can\'t start app. No system tray found')
+            sys.exit(1)
+
+        QApplication.setQuitOnLastWindowClosed(False)
+        # Create the tray
+        tray = SystemTray(settings, stats, parent=app)
+        app.aboutToQuit.connect(tray.prepare_to_exit)
+        app.commitDataRequest.connect(tray.prepare_to_exit)
 
     app.setApplicationName("overwatch-omnic-rewards")
     app.setApplicationVersion("1.1.3")
@@ -56,7 +74,6 @@ def arg_parse():
     group.add_argument("-l", "--log", default="warning", help="Provide logging level")
     parser.add_argument("-fl", "--file-log", nargs='?', const="omnic.log",
                         help="Writes logs to a file. Will write to 'omnic.log' when no filename is provided")
-    parser.add_argument("-q", "--quiet", help="Quiet mode. No system tray", action="store_true")
     parser.add_argument("-d", "--debug",
                         help="Debug Mode. Switches URL's endpoints to local ones for testing. See docs",
                         action="store_true")
@@ -64,6 +81,7 @@ def arg_parse():
                        help="Specify config file. Needs to be in the same dir as app")
     parser.add_argument("-hf", "--history", default="history.csv",
                        help="Specify history file. Needs to be in the same dir as app")
+    parser.add_argument("-c", "--cli", help="Command Line mode. No system tray", action="store_true")
 
     options, qt_args = parser.parse_known_args()
 
@@ -87,6 +105,10 @@ def configure_logging(options, application_path: str):
         raise ValueError(
             f"log level given: {options.log}"
             f" -- must be one of: {' | '.join(levels.keys())}")
+
+    # Set logging to, at least, INFO level when in CLI mode
+    if options.cli and level > logging.INFO:
+        level = logging.INFO
 
     log_handlers = []
 
